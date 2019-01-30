bb0de681ee05c2ddffc239b09cb8582d2181f3f2
CrazzyReeZzy <crazzyhappyend@gmail.com>
2019-01-31 04:29:07 +0500

    Установил gulp

---
+++ app/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <meta name="description" content="">
+    <link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" href="img/favicon/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css.min/var.min.css">
+    <link rel="stylesheet" href="css.min/fonts.min.css">
+    <link rel="stylesheet" href="css.min/header.min.css">
+	<link rel="stylesheet" href="css.min/main.min.css">
+    <link rel="stylesheet" href="css.min/media.min.css">
+</head>
+<body>
+    <?php  echo "Это работает!";?>
+</body>
+</html>
