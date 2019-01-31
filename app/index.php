7361678908400c0d2cd88d73cdef189e59370742
CrazzyReeZzy <crazzyhappyend@gmail.com>
2019-02-01 04:16:19 +0500

    Добавлен слайдер

--- app/index.php
+++ app/index.php
@@ -14,11 +14,15 @@
     <link rel="stylesheet" href="css/var.css">
     <link rel="stylesheet" href="css/fonts.css">
     <link rel="stylesheet" href="css/header.css">
-	<link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="libs/slick/slick.css">
+    <link rel="stylesheet" href="libs/slick/slick-theme.css">
     <link rel="stylesheet" href="css/media.css">
 </head>
 <body>
+    <!-- Подключаем меню !-->
     <?php require_once "php-script/header.php";?>
+    <!-- Первая строка social,logo,phone !-->
     <section class = "offer">
         <div class="wrap-offer">
             <div class="social">
@@ -40,5 +44,20 @@
             </div>
         </div>
     </section>
+    <!-- Слайдер !-->
+    <section class = "slider">
+        <div class="wrap-slider">
+            <div><img src="img/main/slider/slide_one.jpg" alt="Караоке бар - блюда"
+				style="height: 250px; width: 100%;"></div>
+			<div><img src="img/main/slider/slide_two.jpg"  alt="Караоке бар - алкоголь" ></div>
+			<div><img src="img/main/slider/slide_three.jpeg"  alt="Караоке бар - блюда" ></div>
+			<div><img src="img/main/slider/slide_four.jpg"  alt="Караоке бар - В Екатеренбурге" ></div>
+			<div><img src="img/main/slider/slide_five.jpg" alt="Караоке бар - В Екатеренбурге" ></div>
+        </div>
+    </section>
 </body>
+    <!--script !-->
+    <script src="libs/jquery/jquery-1.11.2.min.js"></script>
+    <script src="libs/slick/slick.min.js"></script>
+    <script src="js/slider.js"></script>
 </html>
