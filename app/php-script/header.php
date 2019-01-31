3b159b8775a74114f32284baf2c136fa19fc5011
CrazzyReeZzy <crazzyhappyend@gmail.com>
2019-01-31 05:57:50 +0500

    Оптимизация меню

--- app/php-script/header.php
+++ app/php-script/header.php
@@ -4,16 +4,16 @@
             <input type="checkbox" name="toggle" id="menu" class="toggleMenu">
             <label for="menu" class="toggleMenu"><i class="icon"></i>Меню</label>
             <ul class="menu">
-                <li><a  class="menu__item" href="#">Главная</a></li>
-                <li><a  class="menu__item" href="menu.html">Меню</a></li>
-				<li><a  class="menu__item" href="stock.html">Акции</a></li>
-				<li><a  class="menu__item" href="special offer.html">Спец.Предложения</a></li>
+                <li><a  class="menu__item" href="index.php">Главная</a></li>
+                <li><a  class="menu__item" href="menu.php">Меню</a></li>
+				<li><a  class="menu__item" href="stock.php">Акции</a></li>
+				<li><a  class="menu__item" href="special offer.php">Спец.Предложения</a></li>
 				<li><a  class="menu__item" href="search.php">Каталог&nbsp;песен</a></li>
-				<li><a  class="menu__item" href="menu-gallery.php" class="str">Галерея</a></li>
-				<li><a  class="menu__item" href="aboutus.html">О&nbsp;нас</a></li>
-				<li><a  class="menu__item" href="comment.html">Отзывы</a></li>
-				<li><a  class="menu__item" href="regulation.html">Правила</a></li>
-				<li><a  class="menu__item" href="contacts.html">Контакты</a></li>
+				<li><a  class="menu__item" href="menu-gallery.php">Галерея</a></li>
+				<li><a  class="menu__item" href="aboutus.php">О&nbsp;нас</a></li>
+				<li><a  class="menu__item" href="comment.php">Отзывы</a></li>
+				<li><a  class="menu__item" href="regulation.php">Правила</a></li>
+				<li><a  class="menu__item" href="contacts.php">Контакты</a></li>
             </ul>
         </nav>
     </div>
