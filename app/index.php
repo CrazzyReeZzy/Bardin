3ac912f97354e7c429eb6ebdf581ec56373baa42
CrazzyReeZzy <crazzyhappyend@gmail.com>
2019-01-31 05:07:13 +0500

    Добавил меню

--- app/index.php
+++ app/index.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Bardin</title>
     <meta name="description" content="">
     <link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon">
 	<link rel="apple-touch-icon" href="img/favicon/apple-touch-icon.png">
 	<link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png">
 	<link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png">
     <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css.min/var.min.css">
-    <link rel="stylesheet" href="css.min/fonts.min.css">
-    <link rel="stylesheet" href="css.min/header.min.css">
-	<link rel="stylesheet" href="css.min/main.min.css">
-    <link rel="stylesheet" href="css.min/media.min.css">
+    <link rel="stylesheet" href="css/var.css">
+    <link rel="stylesheet" href="css/fonts.css">
+    <link rel="stylesheet" href="css/header.css">
+	<link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/media.css">
 </head>
 <body>
-    <?php  echo "Это работает!";?>
+    <?php require_once "php-script/header.php";?>
 </body>
 </html>
