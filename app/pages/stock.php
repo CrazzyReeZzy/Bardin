14745c7beef6c29466da8afbc5718452fdb89abb
CrazzyReeZzy <crazzyhappyend@gmail.com>
2019-02-04 23:53:25 +0500

    Добавил акции и адаптацию

---
+++ app/pages/stock.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bardin</title>
+    <meta name="description" content="">
+    <link rel="shortcut icon" href="../img/favicon/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" href="../img/favicon/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="../img/favicon/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="../img/favicon/apple-touch-icon-114x114.png">
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/var.css">
+    <link rel="stylesheet" href="../css/fonts.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/stock.css">
+    <link rel="stylesheet" href="../css/stock-media.css">
+    <link rel="stylesheet" href="../css/footer.css">
+</head>
+<body>
+    <!-- Подключаем меню !-->
+    <?php require_once "../php-script/header.php";?>
+    <!-- Главная секция !-->
+    <main class = "main" id = "main">
+        <h1>Акции</h1>
+        <div class = "wrap-main">
+            <div href="#" class = "wrap-card">
+                <span class="card__img" style="background-image: url(../img/main/special-offers/li1.jpg);"></span>
+                <span class="card__content">
+                    <p>Скидка 25% <br> На меню кухни с вс по вт.</p>
+                    <p class = "content-text">
+                    С воскресенья по вторник в нашем баре проходит замечательная акция! 
+                    Скидка 25% на все меню кухни. Наше меню достаточно разноообразно,
+                    вы сможете попробовать множество блюд, да еще и со скидкой в 25%.</p>
+                </span>
+            </div>
+            <div href="#" class = "wrap-card">
+                <span class="card__img" style="background-image: url(../img/main/special-offers/li2.jpg);"></span>
+                <span class="card__content">
+                    <p>Скидка 25% <br> На меню кухни с вс по вт.</p>
+                    <p class = "content-text">
+                    С воскресенья по вторник в нашем баре проходит замечательная акция! 
+                    Скидка 25% на все меню кухни. Наше меню достаточно разноообразно,
+                    вы сможете попробовать множество блюд, да еще и со скидкой в 25%.</p>
+                </span>
+            </div>
+            <div href="#" class = "wrap-card">
+                <span class="card__img" style="background-image: url(../img/main/special-offers/li3.jpg);"></span>
+                <span class="card__content">
+                    <p>Скидка 25% <br> На меню кухни с вс по вт.</p>
+                    <p class = "content-text">
+                    С воскресенья по вторник в нашем баре проходит замечательная акция! 
+                    Скидка 25% на все меню кухни. Наше меню достаточно разноообразно,
+                    вы сможете попробовать множество блюд, да еще и со скидкой в 25%.</p>
+                </span>
+            </div>
+            <div href="#" class = "wrap-card">
+                <span class="card__img" style="background-image: url(../img/main/special-offers/li4.jpg);"></span>
+                <span class="card__content">
+                    <p>Скидка 25% <br> На меню кухни с вс по вт.</p>
+                    <p class = "content-text">
+                    С воскресенья по вторник в нашем баре проходит замечательная акция! 
+                    Скидка 25% на все меню кухни. Наше меню достаточно разноообразно,
+                    вы сможете попробовать множество блюд, да еще и со скидкой в 25%.</p>
+                </span>
+            </div>
+            <div href="#" class = "wrap-card">
+                <span class="card__img" style="background-image: url(../img/main/special-offers/li5.jpg);"></span>
+                <span class="card__content">
+                    <p>Скидка 25% <br> На меню кухни с вс по вт.</p>
+                    <p class = "content-text">
+                    С воскресенья по вторник в нашем баре проходит замечательная акция! 
+                    Скидка 25% на все меню кухни. Наше меню достаточно разноообразно,
+                    вы сможете попробовать множество блюд, да еще и со скидкой в 25%.</p>
+                </span>
+            </div>
+            <div href="#" class = "wrap-card">
+                <span class="card__img" style="background-image: url(../img/main/special-offers/li6.jpg);"></span>
+                <span class="card__content">
+                    <p>Скидка 25% <br> На меню кухни с вс по вт.</p>
+                    <p class = "content-text">
+                    С воскресенья по вторник в нашем баре проходит замечательная акция! 
+                    Скидка 25% на все меню кухни. Наше меню достаточно разноообразно,
+                    вы сможете попробовать множество блюд, да еще и со скидкой в 25%.</p>
+                </span>
+            </div>
+        </div>
+    </main>
+    <!-- Footer !-->
+    <?php require_once "../php-script/footer.php";?>
+</body>
+</html>
