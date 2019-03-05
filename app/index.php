f67660cf6a237253d46807005aa8780be8956708
CrazzyReeZzy <crazzyhappyend@gmail.com>
2019-03-05 10:49:54 +0500

    Начало измение структуры

--- app/index.php
+++ app/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Bardin</title>
-    <meta name="description" content="">
+    <meta name="description" content="bardin">
     <link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon">
 	<link rel="apple-touch-icon" href="img/favicon/apple-touch-icon.png">
 	<link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png">
@@ -21,39 +21,41 @@
     <link rel="stylesheet" href="css/footer.css">
 </head>
 <body>
-    <!-- Подключаем меню !-->
-    <?php require_once "php-script/header.php";?>
-    <!-- Первая строка social,logo,phone !-->
-    <section class = "offer">
-        <div class="wrap-offer">
-            <div class="social">
-                <a href="https://vk.com/karaokebardin_ekb" class = "social__vk"></a>
-                <a href="https://instagram.com/karaokebardin_ekb" class = "social__instagram"></a>
+    <section class = "fist-bg">
+        <!-- Подключаем меню !-->
+        <?php require_once "php-script/header.php";?>
+        <!-- Первая строка social,logo,phone !-->
+        <section class = "offer">
+            <div class="wrap-offer">
+                <div class="social">
+                    <a href="https://vk.com/karaokebardin_ekb" class = "social__vk"></a>
+                    <a href="https://instagram.com/karaokebardin_ekb" class = "social__instagram"></a>
+                </div>
+                <div class="wrap-logo">
+                    <picture>
+                        <source srcset="img/main/logo-sl.png" class="logo-sl"
+                        media="(max-width: 800px)" >
+					    <img src="img/main/logo-il.png" alt="Golden" class="logo">
+				    </picture>
+                </div>
+                <div class="phone">
+                    <svg class = "phone__icon"></svg>
+                    <p>213-40-20<br>
+                    karaokebard@yandex.ru
+                    </p>
+                </div>
             </div>
-            <div class="wrap-logo">
-                <picture>
-                    <source srcset="img/main/logo-sl.png" class="logo-sl"
-                     media="(max-width: 800px)" >
-					<img src="img/main/logo-il.png" alt="Golden" class="logo">
-				</picture>
+        </section>
+        <!-- Слайдер !-->
+        <section class = "slider">
+            <div class="wrap-slider">
+                <div><img src="img/main/slider/slide_one.jpg" alt="Караоке бар - блюда"></div>
+			    <div><img src="img/main/slider/slide_two.jpg"  alt="Караоке бар - алкоголь" ></div>
+			    <div><img src="img/main/slider/slide_three.jpg"  alt="Караоке бар - блюда" ></div>
+			    <div><img src="img/main/slider/slide_four.jpg"  alt="Караоке бар - В Екатеренбурге" ></div>
+			    <div><img src="img/main/slider/slide_five.jpg" alt="Караоке бар - В Екатеренбурге" ></div>
             </div>
-            <div class="phone">
-                <svg class = "phone__icon"></svg>
-                <p>213-40-20<br>
-                karaokebard@yandex.ru
-                </p>
-            </div>
-        </div>
-    </section>
-    <!-- Слайдер !-->
-    <section class = "slider">
-        <div class="wrap-slider">
-            <div><img src="img/main/slider/slide_one.jpg" alt="Караоке бар - блюда"></div>
-			<div><img src="img/main/slider/slide_two.jpg"  alt="Караоке бар - алкоголь" ></div>
-			<div><img src="img/main/slider/slide_three.jpg"  alt="Караоке бар - блюда" ></div>
-			<div><img src="img/main/slider/slide_four.jpg"  alt="Караоке бар - В Екатеренбурге" ></div>
-			<div><img src="img/main/slider/slide_five.jpg" alt="Караоке бар - В Екатеренбурге" ></div>
-        </div>
+        </section>
     </section>
     <!-- Акции !-->
     <section class = "stock">
--- app/php-script/header.php
+++ app/php-script/header.php
@@ -6,14 +6,10 @@
             <ul class="menu">
                 <li><a  class="menu__item" href="/index.php">Главная</a></li>
                 <li><a  class="menu__item" href="/pages/menu.php">Меню</a></li>
-				<li><a  class="menu__item" href="/pages/stock.php">Акции</a></li>
-				<li><a  class="menu__item" href="/pages/special_offers.php">Спец.Предложения</a></li>
-				<li><a  class="menu__item" href="search.php">Каталог&nbsp;песен</a></li>
-				<li><a  class="menu__item" href="menu-gallery.php">Галерея</a></li>
-				<li><a  class="menu__item" href="aboutus.php">О&nbsp;нас</a></li>
-				<li><a  class="menu__item" href="comment.php">Отзывы</a></li>
-				<li><a  class="menu__item" href="regulation.php">Правила</a></li>
-				<li><a  class="menu__item" href="contacts.php">Контакты</a></li>
+				<li><a  class="menu__item" href="/pages/search.php">Каталог&nbsp;песен</a></li>
+				<li><a  class="menu__item" href="/pages/menu-gallery.php">Фотогаллерея</a></li>
+				<li><a  class="menu__item" href="/pages/contacts.php">Контакты</a></li>
+				<li><a  class="menu__item" href="/pages/menu-gallery.php">Галерея</a></li>
             </ul>
         </nav>
     </div>
