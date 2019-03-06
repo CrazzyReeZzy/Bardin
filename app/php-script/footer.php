f5974f2d32770d0832a9236edd2587d94d781b13
CrazzyReeZzy <crazzyhappyend@gmail.com>
2019-03-06 21:29:43 +0500

    Доверстал главную страницу(без адаптива)

--- app/php-script/footer.php
+++ app/php-script/footer.php
@@ -1,6 +1,5 @@
 <footer class = "footer" id = "footer">
     <div class = "wrap-footer">
-        <svg class = "wrap-footer__line"></svg>
         <div class="contact-flex">
             <p>г.Екатеринбург<br>ул.Академика Бардина 37, 2 этаж.</p>
             <p>Телефон:213-40-20</p>
