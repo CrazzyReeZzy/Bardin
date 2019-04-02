b53eece634a5f69487fb488869339d9fca9c76e6
CrazzyReeZzy <crazzyhappyend@gmail.com>
2019-04-03 00:55:20 +0500

    footer

--- app/php-script/footer.php
+++ app/php-script/footer.php
@@ -1,8 +1,39 @@
 <footer class = "footer" id = "footer">
-    <div class = "wrap-footer">
-        <div class="contact-flex">
-            <p>г.Екатеринбург<br>ул.Академика Бардина 37, 2 этаж.</p>
-            <p>Телефон:213-40-20</p>
+    <div class="wrap-footer">
+        <div class="contact">
+            <div class="adress">
+                <p>г.Екатеринбург<br>ул.Академика Бардина 37, 2 этаж.</p>
+                <p>Телефон:213-40-20</p>
+            </div>
+            <div class="link">
+                <a href="#">Правила</a>
+                <a href="#">Отзывы</a>
+                <a href="pages/contacts.php">Контакты</a>
+            </div>
+        </div>
+        <div class="social">
+            <a href="https://vk.com/karaokebardin_ekb" class = "social__vk"></a>
+            <a href="https://instagram.com/karaokebardin_ekb" class = "social__instagram"></a>
         </div>
     </div>
 </footer>
+
+
+
+<!--<div class = "wrap-footer">
+        <div class="contact-flex">
+            <div class = "contact">
+                <p>г.Екатеринбург<br>ул.Академика Бардина 37, 2 этаж.</p>
+                <p>Телефон:213-40-20</p>
+            </div>
+            <div class="link">
+                <a href="#">Правила</a>
+                <a href="#">Отзывы</a>
+                <a href="pages/contacts.php">Контакты</a>
+            </div>
+            <div class="social">
+                <a href="https://vk.com/karaokebardin_ekb" class = "social__vk"></a>
+                <a href="https://instagram.com/karaokebardin_ekb" class = "social__instagram"></a>
+            </div>
+        </div>
+    </div> !-->
